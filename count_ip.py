d2bbdc37d439eae112bfdcf0ee845867eaf982fa
root <root@VM_53_165_centos.(none)>
2019-03-13 09:49:21 +0800

    count_ip.py

---
+++ count_ip.py
@@ -0,0 +1,18 @@
+计出每个IP的访问量有多少？（从日志文件中查找）
+ 
+#!/usr/bin/python3
+ 
+#!coding=utf-8
+ 
+list = []
+ 
+f = file('/tmp/1.log')
+str1 = f.readlines()
+f.close()
+for i in str1:
+  ip = i.split()[0]
+  list.append(ip)
+  list_num = set(list) 
+for j in list_num:
+   num = list.count(j) 
+print '%s : %s' %(j,num)
